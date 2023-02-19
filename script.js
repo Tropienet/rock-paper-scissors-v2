004f3b112475a6fa85db070f98de5ac6261a5e62
Tin Goricki <exibodude@gmail.com>
2023-02-19 17:22:39 +0100

    Created playRound function

--- script.js
+++ script.js
@@ -29,10 +29,27 @@ function getChoice() {
 
 function playRound( playerChoice, computerChoice ) {
 
-    if( playerChoice.toLowerCase() == computerChoice ) {
+    if( playerChoice.toLowerCase() === computerChoice ) {
         return "tie";
+    } else if( playerChoice.toLowerCase()==="paper" ) {
+       if( computerChoice==="rock" ) {
+        return "You win. Paper beats rock!";
+       } else {
+        return "You lose. Scissors beat paper";
+       }
+    } else if( playerChoice.toLowerCase() === "rock" ) {
+        if ( computerChoice === "scissors" ) {
+            return "You win. Rock beats scissors";
+        } else {
+            return "You lose. Paper beats rock";
+        }
     } else {
-        
+        if( computerChoice === "paper" ) {
+            return "You win. Scissors beat paper";
+        } else {
+            return "You lose. Rock beats scissors";
+        }
     }
-
+    
+    return "Error";
 } 
